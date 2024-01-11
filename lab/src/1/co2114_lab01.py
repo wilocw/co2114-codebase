c90cf53c5a5bbfaa381eae1fb47c6f1646490d22
wil oc ward <wocw2@le.ac.uk>
2024-01-11 19:20:07 +0000

    preparation for lab 1

---
+++ lab/src/1/co2114_lab01.py
@@ -0,0 +1,23 @@
+"""CO2114_LAB01.PY
+
+Use this file to edit your Python code and 
+  try the exercises in Lab 01
+"""
+
+
+## Write your class definitions here
+
+
+## 
+def main():
+    """ Main method for running script code """
+    # write your non-class code here
+    print("Hello World")
+
+
+#################################
+## DO NOT EDIT BELOW THIS LINE ##
+##   UNLESS YOU KNOW WHAT YOU  ##
+##     ARE DOING               ##
+if __name__ == "__main__":
+    main() 
